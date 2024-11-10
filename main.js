89540c530af721ad636e9f00cdd3f4f6aa2634d7
Itai Katz <katzitai96@gmail.com>
2024-11-10 16:14:23 +0200

    complete first version of website

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-console.log("Hello World!");
-setupCounter();
+setupTransition();
 
-function setupCounter() {
-  let count = 0;
+function setupTransition() {
 
-  function increment() {
-    count++;
-    document.querySelector("#count").innerHTML = count;
+  function changeClass1(){
+    if(document.querySelector("#arnona_problem_1").className == "common_problem_hover"){
+      document.querySelector("#arnona_problem_1").className = "common_problem";
+    }
+    else{
+      document.querySelector("#arnona_problem_1").className = "common_problem_hover";
+    }
   }
 
-  function decrement() {
-    count--;
-    document.querySelector("#count").innerHTML = count;
+  function changeClass2(){
+    if(document.querySelector("#arnona_problem_2").className == "common_problem_hover"){
+      document.querySelector("#arnona_problem_2").className = "common_problem";
+    }
+    else{
+      document.querySelector("#arnona_problem_2").className = "common_problem_hover";
+    }
   }
 
-  document.querySelector("#increment").addEventListener("click", increment);
-  document.querySelector("#decrement").addEventListener("click", decrement);
+  function changeClass3(){
+    if(document.querySelector("#arnona_problem_3").className == "common_problem_hover"){
+      document.querySelector("#arnona_problem_3").className = "common_problem";
+    }
+    else{
+      document.querySelector("#arnona_problem_3").className = "common_problem_hover";
+    }
+  }
+
+  document.querySelector("#arnona_problem_1").addEventListener("mouseenter", changeClass1);
+  document.querySelector("#arnona_problem_1").addEventListener("mouseleave", changeClass1);
+
+  document.querySelector("#arnona_problem_2").addEventListener("mouseenter", changeClass2);
+  document.querySelector("#arnona_problem_2").addEventListener("mouseleave", changeClass2);
+
+  document.querySelector("#arnona_problem_3").addEventListener("mouseenter", changeClass3);
+  document.querySelector("#arnona_problem_3").addEventListener("mouseleave", changeClass3);
 }
